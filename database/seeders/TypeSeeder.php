44c924f83643db17700ec03c7e60742a831a62ac
andreacassini <andrea1316@hotmail.it>
2023-08-28 12:35:16 +0200

    seeded db

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use App\Models\Type;
 
 class TypeSeeder extends Seeder
 {
@@ -14,6 +16,15 @@ class TypeSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $types = ['Frontend', 'Backend', 'Fullstack', 'Designer', 'DevOps'];
+
+        foreach ($types as $typ) {
+            $type = new Type();
+
+            $type->name = $typ;
+            $type->slug = Str::slug($typ, '-');
+
+            $type->save();
+        }
     }
 }
