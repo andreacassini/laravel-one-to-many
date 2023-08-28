813ab5316a9e7d94bc3b37fd878452b57836f4de
andreacassini <andrea1316@hotmail.it>
2023-08-28 12:40:27 +0200

    Add methods Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -6,11 +6,17 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 use Illuminate\Support\Str;
+use App\Models\Type;
 
 class Post extends Model
 {
     use HasFactory;
-    protected $fillable = ['title', 'content', 'slug', 'cover_image'];
+    protected $fillable = ['title', 'content', 'slug', 'cover_image', 'type_id'];
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 
     public static function generateSlug($title)
     {
