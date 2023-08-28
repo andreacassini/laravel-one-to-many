ace2d793622f2949c8aa7fe62ddd3a51b9588636
andreacassini <andrea1316@hotmail.it>
2023-08-28 12:14:12 +0200

    Add function up table posts

---
+++ database/migrations/2023_08_28_100747_add_type_id_column_to_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //DEFINISCO LA COLONNA COME unsignedBigInteger
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+
+            //CREO VINCOLO/FOREIGN KEY
+            $table->foreign('type_id')->references('id')->on('type')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
